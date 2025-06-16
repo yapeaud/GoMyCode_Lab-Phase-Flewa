d00fb94c68f2b3404d7c8967ca5d34c4fe1c24ff
yapeaud <yapoabed@gmailcom>
2025-06-16 15:05:41 +0000

    Renforcement de la logique dans les interfaces Frontend et les systèmes Backend.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,6 +12,8 @@ import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -22,6 +25,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", flewaRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4010/api/" : "/api";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:4010/api/",
+    baseURL: BASE_URL,
     withCredentials: true // Envoie des cookies avec la requête
 });
